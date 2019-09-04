7a92ad34beae89a34dab567c6b1a5f7ec3ba6a49
cecileanne <cecileannesison@gmail.com>
2019-09-04 17:34:18 -0500

    wrapped the code into two functions - make a button and make a button clickable

--- script.js
+++ script.js
@@ -16,57 +16,65 @@ const topics = [
   `Vulpix`
 ];
 
-// Function to intialize the page 
+// Function to intialize the page
 function initializeTopicButtons() {
-// For Loop - Create buttons for each index of the array to populate #buttonRow
-for (
-  let topicPokemonIndex = 0;
-  topicPokemonIndex < topics.length;
-  topicPokemonIndex++
-) {
-  let buttonHTML = 
-    "<button class=`topicButton` data-pokemon= + topics[topicPokemonIndex] + >" +
+  // For Loop - Create buttons for each index of the array to populate #buttonRow
+  for (
+    var topicPokemonIndex = 0;
+    topicPokemonIndex < topics.length;
+    topicPokemonIndex++
+  ) {
+    let buttonHTML =
+      "<button class='topicButton' data-pokemon='topics[topicPokemonIndex]'>" +
       topics[topicPokemonIndex] +
-      "</button>"
-  );
-  $(buttonHTML).appendTo("#buttonRow");
-}
+      "</button>";
+    $(buttonHTML).appendTo("#buttonRow");
+  }
 }
 
-// For Loop -  When users click on a button in #buttonRow will send a queryURL to giphy, limit of 10, no autoplay
-$("#buttonRow").on("click", function() {
-  const queryButton = $(this).attr("data-pokemon");
-  const queryURL =
-    "https://api.giphy.com/v1/gifs/search?q=" +
-    queryButton +
-    "&api_key=VtFYkxuLNqaMPJL3tTXdA3ekk66Q5tJg&limit=10";
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    // Create a variable named results and set it equal to response.data
-    const results = response.data;
+// Function to make the buttons clickable:
+function makeTopicButtonsClickable(selector) {
+  // For Loop -  When users click on a button in #buttonRow will send a queryURL to giphy, limit of 10, no autoplay
+  $(selector).on("click", function() {
+    let queryButton = $(this).attr("data-pokemon");
+    let queryURL =
+      "https://api.giphy.com/v1/gifs/search?q=" +
+      queryButton +
+      "&api_key=VtFYkxuLNqaMPJL3tTXdA3ekk66Q5tJg&limit=10";
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      // Create a variable named results and set it equal to response.data
+      const results = response.data;
 
-    // For Loop - Send the response aka the results to the page in id #gifPanel
-    for (const index = 0; index < results.length; index++) {
-      // by creating a div (variable pokemonDiv)
-      let pokemonDiv = $(`<div></div>`);
-      // Make a paragraph tag with jQuery and store it in a variable named p.
-      let p = $(`<p></p>`);
-      // Set the inner text of the paragraph to the rating of the image in results[i].
-      $(`p`).text(results[index].rating);
-      // Make an image tag with jQuery and store it in a variable named pokemonImage.
-      let pokemonImage = $(`<img>`);
-      // Set the image's src to results[i]'s fixed_height.url (but use still since we want to do on click).
-      pokemonImage.attr("src", results[index].images.fixed_height_still.url);
-      // Append the p variable to the pokemonDiv variable.
-      pokemonDiv.append(p);
-      // Append the animalImage variable to the pokemonDiv variable.
-      pokemonDiv.append(pokemonImage);
-      // Prepend the pokemonDiv variable to the element with an id of gifPanel.
-      $(`#gifPanel`).prepend(pokemonDiv);
-    }
+      // For Loop - Send the response aka the results to the page in id #gifPanel
+      for (const index = 0; index < results.length; index++) {
+        // by creating a div (variable pokemonDiv)
+        let pokemonDiv = $(`<div></div>`);
+        // Make a paragraph tag with jQuery and store it in a variable named p.
+        let p = $(`<p></p>`);
+        // Set the inner text of the paragraph to the rating of the image in results[i].
+        $(`p`).text(`Rating: ` + results[index].rating);
+        // Make an image tag with jQuery and store it in a variable named pokemonImage.
+        let pokemonImage = $(`<img>`);
+        // Set the image's src to results[i]'s fixed_height.url (but use still since we want to do on click).
+        pokemonImage.attr("src", results[index].images.fixed_height_still.url);
+        // Append the p variable to the pokemonDiv variable.
+        pokemonDiv.append(p);
+        // Append the animalImage variable to the pokemonDiv variable.
+        pokemonDiv.append(pokemonImage);
+        // Prepend the pokemonDiv variable to the element with an id of gifPanel.
+        $(`#gifPanel`).prepend(pokemonDiv);
+      }
+    });
   });
+}
+
+// Run this to make the buttons
+$(document).ready(function() {
+  initializeTopicButtons();
+  makeTopicButtonsClickable("button.topicbutton");
 });
 
 // // When users add in a search term for additional Pokemon, clicking on the #addButton will append the #searchTag (user input) as a string to the array
