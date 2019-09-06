0f89f1953f86ae5c5deb139785d2ee943b8b6f70
cecileanne <cecileannesison@gmail.com>
2019-09-06 16:04:22 -0500

    created some attributes for the image divs so we could write an if else statement on the click

--- script.js
+++ script.js
@@ -1,106 +1,116 @@
-// Create an array of pokemon
-const topics = [
-  `Bidoof`,
-  `Cloyster`,
-  `Jynx`,
-  `Mareep`,
-  `Mesprit`,
-  `Metapod`,
-  `Politoed`,
-  `Slakoth`,
-  `Snorlax`,
-  `Squirtle`,
-  `Suicune`,
-  `Tangela`,
-  `Venomoth`,
-  `Vulpix`
-];
+// Run this to make the buttons - should be at the top and close at the very bottom
+$(document).ready(function() {
+  // Create an array of pokemon
+  const topics = [
+    `Bidoof`,
+    `Cloyster`,
+    `Jynx`,
+    `Mareep`,
+    `Mesprit`,
+    `Metapod`,
+    `Politoed`,
+    `Slakoth`,
+    `Snorlax`,
+    `Squirtle`,
+    `Suicune`,
+    `Tangela`,
+    `Venomoth`,
+    `Vulpix`
+  ];
 
-// Function to intialize the page
-function initializeTopicButtons() {
-  // For Loop - Create buttons for each index of the array to populate #buttonRow
-  $("#buttonRow").empty();
-  for (
-    var topicPokemonIndex = 0;
-    topicPokemonIndex < topics.length;
-    topicPokemonIndex++
-  ) {
-    let buttonHTML =
-      "<button class='topicButton'>" + topics[topicPokemonIndex] + "</button>";
-    $(".topicButton").attr("data-pokemon", topics[topicPokemonIndex]),
-      $(buttonHTML).appendTo("#buttonRow");
+  // Received errors if placed above the array
+  initializeTopicButtons();
+
+  // Function to intialize the page
+  function initializeTopicButtons() {
+    // For Loop - Create buttons for each index of the array to populate #buttonRow
+    $("#buttonRow").empty();
+    for (
+      var topicPokemonIndex = 0;
+      topicPokemonIndex < topics.length;
+      topicPokemonIndex++
+    ) {
+      let buttonHTML =
+        "<button class='topicButton'>" +
+        topics[topicPokemonIndex] +
+        "</button>";
+      $(".topicButton").attr("data-pokemon", topics[topicPokemonIndex]),
+        $(buttonHTML).appendTo("#buttonRow");
+    }
   }
-}
 
-// // Function to make the buttons clickable:
-// function makeTopicButtonsClickable(selector) {
-// For Loop -  When users click on a button in #buttonRow will send a queryURL to giphy, limit of 10, no autoplay
-$(document).on("click", `.topicButton`, function() {
-  let queryButton = $(this).text();
-  console.log(queryButton);
-  let queryURL =
-    "https://api.giphy.com/v1/gifs/search?q=" +
-    queryButton +
-    "&api_key=VtFYkxuLNqaMPJL3tTXdA3ekk66Q5tJg&limit=10";
-  console.log(queryURL);
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    // Create a variable named results and set it equal to response.data
-    const results = response.data;
+  // For Loop -  When users click on a button in #buttonRow will send a queryURL to giphy, limit of 10, no autoplay
+  $(document).on("click", `.topicButton`, function() {
+    let userInput = $(this).text();
+    let queryURL =
+      "https://api.giphy.com/v1/gifs/search?q=" +
+      userInput +
+      "&api_key=VtFYkxuLNqaMPJL3tTXdA3ekk66Q5tJg&limit=10";
+    // console.log(queryURL);
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      // Create a variable named results and set it equal to response.data
+      const results = response.data;
 
-    // For Loop - Send the response aka the results to the page in id #gifPanel
-    for (let index = 0; index < results.length; index++) {
-      // by creating a div (variable pokemonDiv)
-      let pokemonDiv = $(`<div></div>`);
-      // Make a paragraph tag with jQuery and store it in a variable named p.
-      let p = $(`<p></p>`);
-      // Set the inner text of the paragraph to the rating of the image in results[i].
-      $(`p`).text(`Rating: ` + results[index].rating);
-      // Make an image tag with jQuery and store it in a variable named pokemonImage.
-      let pokemonImage = $(`<img class="imageCall">`);
-      let pokemonImageAnimated = $(`<img class="imageCall">`);
-      // Set the image's src to results[i]'s fixed_height.url (but use still since we want to do on click).
-      pokemonImage.attr("src", results[index].images.fixed_height_still.url);
-      pokemonImageAnimated.attr("src", results[index].images.fixed_height.url);
-      // Append the p variable to the pokemonDiv variable.
-      pokemonDiv.append(p);
-      // Append the animalImage variable to the pokemonDiv variable.
-      pokemonDiv.append(pokemonImage);
-      // Prepend the pokemonDiv variable to the element with an id of gifPanel.
-      $(`#gifPanel`).prepend(pokemonDiv);
+      // For Loop - Send the response aka the results to the page in id #gifPanel
+      for (let index = 0; index < results.length; index++) {
+        // by creating a div (variable pokemonDiv)
+        let pokemonDiv = $(`<div>`);
+        // Make a paragraph tag with jQuery and store it in a variable named p.
+        let p = $(`<p></p>`);
+        // Set the inner text of the paragraph to the rating of the image in results[i].
+        $(`p`).text(`Rating: ` + results[index].rating);
+        // Make an image tag with jQuery and store it in a variable named pokemonImage.
+        let pokemonImage = $(`<img class="imageCall">`);
+        // Set the image's src to results[i]'s fixed_height.url (but use still since we want to do on click).
+        pokemonImage
+          .attr("data-animated", results[index].images.fixed_height.url)
+          .attr("alt", "animated pokemon")
+          .attr("src", results[index].images.fixed_height_still.url)
+          .attr("data-still", results[index].images.fixed_height_still.url)
+          .attr("alt", "still pokemon")
+          .attr("data-state", "still");
+        // Append the p variable to the pokemonDiv variable.
+        pokemonDiv.append(p);
+        // Append the animalImage variable to the pokemonDiv variable.
+        pokemonDiv.append(pokemonImage);
+        // Prepend the pokemonDiv variable to the element with an id of gifPanel.
+        $(`#gifPanel`).prepend(pokemonDiv);
+      }
+    });
+  });
+  // }
+  // When users click on a still image back on and off - on click should toggle between results[index].images.fixed_height_still.url and results[index].images.fixed_height.url
+  $(document).on("click", `.imageCall`, function() {
+    // TO DO: console.log this to verify this
+    //  console.log($(this));
+    if ($(this).attr("data-state") == "still") {
+      $(this)
+        .attr("src", $(this).attr("data-animated"))
+        .attr("data-state", "animated")
+        .attr("alt", "animated pokemon");
+    } else {
+      $(this)
+        .attr("src", $(this).attr("data-still"))
+        .attr("data-state", "still")
+        .attr("alt", "still pokemon");
     }
   });
-});
-// }
-// TO DO: When users click on a still image back on and off - on click should toggle between results[index].images.fixed_height_still.url and results[index].images.fixed_height.url
-// $(pokemonDiv).on("click") {
-//   if pokemonImage.attr("src", results[index].images.fixed_height_still.url) = true;
-//   $(".imageCall").empty();
-// pokemonDiv.append(pokemonImageAnimated);
-// } else {
-//   $(".imageCall").empty();
-//   pokemonDiv.append(pokemonImage);
-// }
 
-// Run this to make the buttons
-$(document).ready(function() {
-  initializeTopicButtons();
-  // makeTopicButtonsClickable("button.topicButton");
-});
-
-// // When users add in a search term for additional Pokemon, clicking on the #addButton will append the #searchTag (user input) as a string to the array
-// //  On Click event associated with the addButton
-$("#addButton").on("click", function(event) {
-  event.preventDefault();
-  //  Get user's "value" from the textbox and store it a variable
-  const newTopic = $("#userSearchTag")
-    .val()
-    .trim();
-  //  Adds new topic to the topics array
-  topics.push(newTopic);
-  initializeTopicButtons();
-  //  Clear the #userSearchTag
-  $("#userSearchTag").val("");
+  // // When users add in a search term for additional Pokemon, clicking on the #addButton will append the #searchTag (user input) as a string to the array
+  // //  On Click event associated with the addButton
+  $("#addButton").on("click", function(event) {
+    event.preventDefault();
+    //  Get user's "value" from the textbox and store it a variable
+    const newTopic = $("#userSearchTag")
+      .val()
+      .trim();
+    //  Adds new topic to the topics array
+    topics.push(newTopic);
+    initializeTopicButtons();
+    //  Clear the #userSearchTag
+    $("#userSearchTag").val("");
+  });
 });
